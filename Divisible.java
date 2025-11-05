b84c28bd544a42420f1bdbdbf8d9b8b84f65f00d
Ankamma-Raju0000 <ankammaraju0000@gmail.com>
2025-11-05 14:43:37 +0000

    Create Divisible.java

---
+++ Divisible.java
@@ -0,0 +1,13 @@
+package org;
+
+public class Divisible 
+{
+	public static void main(String[] args)
+	{
+		 int a =6;
+		 System.out.println(a%3==0?"divisible by 3":"not divisible by 3");
+		    
+		
+	}
+
+}
